a01cfae01b589cf9bd5685ad0e07250b2e277df7
root <root@srv1016050.hstgr.cloud>
2026-05-24 19:46:06 +0000

    fix: remove error boundary and inject webpack build ignore criteria

--- apps/storefront/next.config.js
+++ apps/storefront/next.config.js
@@ -2,9 +2,6 @@ const checkEnvVariables = require("./check-env-variables")
 
 checkEnvVariables()
 
-/**
- * Medusa Cloud-related environment variables
- */
 const S3_HOSTNAME = process.env.MEDUSA_CLOUD_S3_HOSTNAME
 const S3_PATHNAME = process.env.MEDUSA_CLOUD_S3_PATHNAME
 
@@ -23,6 +20,16 @@ const nextConfig = {
   },
   typescript: {
     ignoreBuildErrors: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.ignoreWarnings = [
+        { module: /not-found/ },
+        { module: /_error/ },
+        { module: /error/ }
+      ]
+    }
+    return config
   },
   images: {
     unoptimized: true,
