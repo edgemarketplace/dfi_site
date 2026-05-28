68026cd84de00088ad65c7ff2e42c488e9d6c0e5
root <root@srv1016050.hstgr.cloud>
2026-05-28 01:22:16 +0000

    fix: type _error page props to avoid implicit any

--- apps/storefront/src/pages/_error.tsx
+++ apps/storefront/src/pages/_error.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
 
-function ErrorOverride({ statusCode }) {
+type ErrorOverrideProps = {
+  statusCode?: number
+}
+
+type ErrorInitialPropsContext = {
+  res?: { statusCode?: number }
+  err?: { statusCode?: number }
+}
+
+function ErrorOverride({ statusCode }: ErrorOverrideProps) {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: 'screen', fontFamily: 'sans-serif' }}>
       <p>{statusCode ? `An error ${statusCode} occurred on server` : 'An error occurred on client'}</p>
@@ -8,7 +17,7 @@ function ErrorOverride({ statusCode }) {
   )
 }
 
-ErrorOverride.getInitialProps = ({ res, err }) => {
+ErrorOverride.getInitialProps = ({ res, err }: ErrorInitialPropsContext) => {
   const statusCode = res ? res.statusCode : err ? err.statusCode : 404
   return { statusCode }
 }
